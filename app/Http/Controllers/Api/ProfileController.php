9b82dec5f6d475a0ee2b93679c3ec2b9f15ccd7e
Hasan <hsmusayev@gmail.com>
2023-04-07 17:10:49 +0400

    first commit

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AllPostsCollection;
+use App\Http\Resources\UserCollection;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+
+    /**
+     * Display the specified resource.
+     *
+     */
+    public function show($id)
+    {
+        try {
+            $posts = Post::where('user_id', $id)->orderBy('created_at', 'desc')->get();
+            $user = User::where('id', $id)->get();
+
+            return response()->json([
+                'posts' => new AllPostsCollection($posts),
+                'user' => new UserCollection($user)
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+}
